b131077ec312edf3fc8c8c2da846f7e6d249d89e
luke <luke@bfc9043e-4c6b-4c86-8dee-79e534635875>
2012-06-30 21:49:29 +0000

    scrolling takes into account where the mouse is!
    git-svn-id: file:///home/luke/git/JavaFractalsvn@3 bfc9043e-4c6b-4c86-8dee-79e534635875

--- src/fractal/Fractal.java
+++ src/fractal/Fractal.java
@@ -7,6 +7,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import LukesBits.*;
+import java.awt.Point;
 
 /**
  *
@@ -15,112 +16,139 @@ import LukesBits.*;
 public class Fractal {
 
     private FractalWindow window;
-    private int width,height, detail;
+    private int width, height, detail;
     private BufferedImage buffer;
     //private Colour black = new Colour(0,0,0);
     private Vector centre;
     private double zoom;
-    
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Fractal f = new Fractal(600,600);
+        Fractal f = new Fractal(600, 600);
     }
-    
-    public Fractal(int _width, int _height){
-        width=_width;
+
+    public Fractal(int _width, int _height) {
+        width = _width;
         height = _height;
-        
-        detail=50;
-        
+
+        detail = 50;
+
         //origin = new Vector((double)width*0.75,(double)height/2.0);
         //zoom = 2.0/(double)width;
-        
+
         //the length of the real axis which stretches across the screen
         zoom = 3.0;
         //what value on the complex plain is in the centre of the screen
-        centre = new Vector(-0.5,0);
-        
-        
-        buffer =  new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
-        
+        centre = new Vector(-0.5, 0);
+
+
+        buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
         generate();
-        
+
         window = new FractalWindow(this, width, height);
-        
+
         window.setVisible(true);
     }
-    
-    public Colour iterationToColour(int i){
 
-        Colour c = Colour.hsvToRgb((double)(i%50)/50.0, 0.5, 1.0);
+    public Colour iterationToColour(int i) {
+
+        Colour c = Colour.hsvToRgb((double) (i % 50) / 50.0, 0.5, 1.0);
         return c;
     }
-    
+
     //taking ints where they are either -1,0 or 1, so the current zoom level is taken into account
-    public void move(int x, int y){
-        centre=centre.add(new Vector(x,y),(zoom*0.1));
+    public void move(int x, int y) {
+        centre = centre.add(new Vector(x, y), (zoom * 0.1));
         generate();
     }
-    
-    public void scroll(double scroll){
-        //zoom+=scroll/500;
-        if(scroll < 0){
-            zoom*=0.9;
-        }else{
-            zoom/=0.9;
+
+    public void scroll(double scroll) {
+        Point m = window.getMousePosition();
+
+        if (m != null) {
+            Vector mouseScreen = new Vector(m.x, m.y);
+
+            //mouse position on the complex plain
+            Vector mouseComplex = offset().add(mouseScreen.multiply(zoom / (double) width));
+
+            //difference between centre and mousecomplex
+            //difference in ratios of zoom
+            //subtract centre the difference * the change in zoom
+
+            if (scroll < 0) {
+                zoom *= 0.9;
+            } else {
+                zoom /= 0.9;
+            }
+
+            Vector newOffset = mouseComplex.subtract(mouseScreen.multiply(zoom / (double) width));
+            centre = newOffset.add(new Vector(zoom / 2.0, zoom / 2.0));
+        } else {
+            if (scroll < 0) {
+                zoom *= 0.9;
+            } else {
+                zoom /= 0.9;
+            }
         }
         generate();
-        
+
     }
-    
-    public void generate(){
-        for(int x=0;x<width;x++){
-            for(int y=0;y<height;y++){
+
+    private Vector offset() {
+        Vector offset = centre.subtract(new Vector(1, 1).multiply(zoom / 2.0));
+
+        return offset;
+    }
+
+    public void generate() {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
                 Colour colour;
-                
-                Vector p = new Vector(x,height-y-1);
-                
+
+                Vector p = new Vector(x, height - y - 1);
+
                 //get p to be relative to offset in the complex plain
-                p=p.multiply(zoom/(double)width);
-                
+                p = p.multiply(zoom / (double) width);
+
                 //offset is the top left on the viewport on the complex plain
-                Vector offset = centre.subtract(new Vector(1,1).multiply(zoom/2.0));
-                p=p.add(offset);
+                //Vector offset = offset();
+                p = p.add(offset());
                 //p=p.subtract(new Vector(zoom/2.0,zoom/2.0));//origin.add(
-                
-                
-                Complex c = new Complex(p.x,p.y);
-                
-                Complex z = new Complex(0,0);
-                
-                int i=0;
-                
-                while(z.magnitudeSqrd() < 4 && i<detail){
+
+
+                Complex c = new Complex(p.x, p.y);
+
+                Complex z = new Complex(0, 0);
+
+                int i = 0;
+
+                while (z.magnitudeSqrd() < 4 && i < detail) {
                     z = z.times(z).plus(c);
                     i++;
                 }
-                
-                if(i>=detail){
+
+                if (i >= detail) {
                     //black for not maxed out yet
-                    colour=new Colour(0,0,0);
-                    
-                }else{
+                    colour = new Colour(0, 0, 0);
+
+                } else {
                     colour = iterationToColour(i);
                 }
-                
-                buffer.setRGB(x, height-y-1, colour.toColor().getRGB());
+
+                buffer.setRGB(x, height - y - 1, colour.toColor().getRGB());
             }
         }
     }
-    
-    public void draw(Graphics g){//,int width,int height
+
+    public void draw(Graphics g) {//,int width,int height
         //Graphics2D g = (Graphics2D) _g;
-        
+
         g.drawImage(buffer, 0, 0, null);
-        
-        g.drawString("Centre: "+centre+" Zoom: "+zoom, 10, 50);
+
+        g.drawString("Centre: " + centre + " Zoom: " + zoom, 10, 50);
     }
 }
